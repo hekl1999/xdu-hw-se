ceeca0305f7a2caf2b30c0835e7ee5a70d0583e1
oneSaber <1357860084@qq.com>
2018-05-08 21:49:56 +0800

    8/5/2018

---
+++ 后端/app/models.py
@@ -0,0 +1,261 @@
+from . import db
+from flask_login import UserMixin, AnonymousUserMixin, login_manager
+from flask import current_app
+from datetime import datetime
+
+
+# 账户表
+class Account(UserMixin, AnonymousUserMixin, db.Model):
+    __tablename__ = 'accounts'
+    account = db.Column(
+        db.String(32),
+        unique=True,
+        primary_key=True,
+        nullable=False,
+        index=True)
+    password = db.Column(db.String(32), nullable=False)
+    type = db.Column(db.String(16), nullable=False)
+
+
+@login_manager.user_loader
+def load_user(account):
+    return Account.query.get(int(account))
+
+
+class AnonymousUser(AnonymousUserMixin):
+    def can(self):
+        return False
+
+    def is_admin(self):
+        return False
+
+
+# 学生选课的表
+class Curricula_variable(db.Model):
+    __tablename__ = 'curricula_variables'
+    student_id = db.Column(
+        db.String(32),
+        db.ForeignKey('students.id'),
+        nullable=False,
+        primary_key=True)
+    class_id = db.Column(
+        db.String(64),
+        db.ForeignKey('classes.id'),
+        nullable=False,
+        primary_key=True)
+    grade = db.Column(db.Integer, nullable=False)
+
+
+# 教学表
+class Teach(db.Model):
+    __tablename__ = 'teaches'
+    instructor_id = db.Column(
+        db.String(32),
+        db.ForeignKey('instructors.id'),
+        nullable=False,
+        primary_key=True)
+    class_id = db.Column(
+        db.String(64),
+        db.ForeignKey('classes.id'),
+        nullable=False,
+        priamry_key=True)
+
+
+# 课程安排表
+class Schedule(db.Model):
+    __tablename__ = 'schedules'
+    class_id = db.Column(
+        db.String(64),
+        db.ForeignKey('classes.id'),
+        nullable=False,
+        primary_key=True)
+    classroom_id = db.Column(
+        db.String(32),
+        db.ForeignKey('classrooms.id'),
+        nullable=False,
+        primary_key=True)
+    week = db.Column(db.Integer, nullable=False)
+    day = db.Column(db.Integer, nullable=False)
+    section = db.Column(db.Integer, nullable=False)
+
+
+# 考场安排表
+class Exam_room(db.Model):
+    __tablename__ = 'exam_rooms'
+    exam_id = db.Column(
+        db.String(16),
+        db.ForeignKey('exams.id'),
+        nullable=False,
+        primary_key=True,
+        index=True)
+    classroom_id = db.Column(
+        db.String(32),
+        db.ForeignKey('classrooms.id'),
+        nullable=False,
+        primary_key=True,
+        index=True)
+
+
+# 考试成绩表
+class Take_exam(db.Model):
+    __tablename__ = 'take_exams'
+    exam_id = db.Column(
+        db.String(16),
+        db.ForeignKey('exams.id'),
+        nullable=False,
+        primary_key=True,
+        index=True)
+    student_id = db.Column(
+        db.String(32),
+        db.ForeignKey('students.id'),
+        nullable=False,
+        primary_key=True,
+        index=True)
+    exam_grade = db.Column(db.Integer)
+
+# 学生表
+
+
+class Student(db.Model):
+    __tablename__ = 'students'
+    id = db.Column(
+        db.String(32),
+        unique=True,
+        primary_key=True,
+        nullable=False,
+        index=True)
+    name = db.Column(db.String(32), nullable=False)
+    year = db.Coulmn(db.String(32), nullable=False)
+    curricula_student = db.relationship(
+        'Curricula_variable',
+        foreign_keys=[
+            Curricula_variable.student_id],
+        backref=db.backref(
+            'student',
+            lazy='joined'),
+        lazy='dynamic',
+        cascade='all, delete-orphan')
+    exam_student = db.relatioship('Take_exam',
+                                  foreign_keys=[Take_exam.student_id],
+                                  backref=db.backref('student', lazy='joined'),
+                                  lazy='dynamic',
+                                  cascade='all, delete-orphan')
+
+    # 学生当前的年级
+    def grade(self):
+        return datetime.now().year - self.year
+
+
+# 教师表
+class Instructor(db.Model):
+    __tablename__ = 'instructors'
+    id = db.Column(db.String(32), primary_key=True, nullable=False, index=True)
+    name = db.Column(db.String(32), nullable=False)
+    teach_teacher = db.relationship(
+        'Teach',
+        foreign_keys=[Teach.instructor_id],
+        backref=db.backref('teacher', lazy='joined'),
+        lazy='dynamic',
+        cascade='all, delete-orphan'
+    )
+
+# 课程表
+
+
+class Course(db.Model):
+    __tablename__ = 'courses'
+    id = db.Coolumn(
+        db.String(32),
+        primary_key=True,
+        nullable=False,
+        index=True,
+        unique=True)
+    name = db.Column(db.String(64), nullable=False, index=True)
+    type = db.Column(db.String(16), nullable=False, index=True)
+    credit = db.Column(db.Integer, nullable=False)
+    period = db.Column(db.Integer, nullable=False)
+    classes = db.relationship('Class', backref='course', lazy='dynamic')
+
+
+# 教室
+class Classroom(db.Model):
+    __tablename__ = 'classrooms'
+    id = db.Column(db.String(32), primary_key=True, nullable=False, index=True)
+    building = db.Column(db.String(32), nullable=False)
+    floor = db.Column(db.Integer, nullable=False)
+    number = db.Column(db.Integer, nullable=False)
+    schedule_classroom = db.relationship(
+        'Schedule',
+        foreign_keys=[
+            Schedule.classroom_id],
+        backref=db.backref(
+            'classroom',
+            lazy='joined'),
+        lazy='dynamic',
+        cascade='all, delete-orphan')
+    exam_classroom = db.relationship(
+        'Exam_room',
+        foreign_keys=[
+            Exam_room.classroom_id],
+        backref=db.backref(
+            'classroom',
+            lazy='joined'),
+        lazy='dynamic',
+        cascade='all, delete-orphan')
+
+# 教学班表
+
+
+class Class(db.Model):
+    __tablename__ = 'classes'
+    id = db.Column(db.String(64), priamry_key=True, index=True, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    term = db.Column(db.Integer, nullable=False)
+    course_id = db.Column(db.String(64), db.ForeignKey('courses.id'))
+    number = db.Column(db.Integer, nullable=False)
+    exam = db.relationship('Exam', backref='class', lazy='dynamic')
+    choose = db.Column(db.Boolean)  # 课程是否可选
+    curricula_class = db.relationship(
+        'Curricula_variable',
+        foreign_keys=[
+            Curricula_variable.class_id],
+        backref=db.backref(
+            'class',
+            lazy='joined'),
+        lazy='dynamic',
+        cascade='all, delete-orphan')
+    teach_class = db.relationship('Teach',
+                                  foreign_keys=[Teach.class_id],
+                                  backref=db.backref('class', lazy='joined'),
+                                  lazy='dynamic',
+                                  cascade='all, delete-orphan')
+    schedule_class = db.relationship(
+        'Schedule',
+        foreign_keys=[
+            Schedule.class_id],
+        backref=db.backref(
+            'class',
+            lzay='joined'),
+        lazy='dynamic',
+        cascade='all,delete-orphan')
+
+
+# 考试表
+class Exam(db.Model):
+    __tablename__ = 'exams'
+    id = db.Column(db.String(16), primary_key=True, index=True)
+    class_id = db.Column(
+        db.String(64),
+        db.ForeignKey('classes.id'),
+        nullable=False)
+    datetime = db.Column(db.DateTime, nullable=False)
+    exam_room = db.relationship('Exam_room',
+                                froeign_keys=[Exam_room.exam_id],
+                                backref=db.backref('exam', lazy='joined'),
+                                lazy='dynamic',
+                                cascade='all, delete-orphan')
+    exam_stu = db.relationship('Take_exam',
+                               foreign_key=[Take_exam.exam_id],
+                               backref=db.backref('exam', lazy='joined'),
+                               lazy='dynamic',
+                               cascade='all,delete-orphan')
