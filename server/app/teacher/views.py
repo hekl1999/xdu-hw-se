d7b898f8a883b7cacba464e3e99acf11e145c37d
oneSaber <1357860084@qq.com>
2018-05-27 16:26:40 +0800

    3

--- server/app/teacher/views.py
+++ server/app/teacher/views.py
@@ -9,12 +9,13 @@ import json
 @teacher.route('/mine_class')
 @login_required
 def mine_class():
+    print(request.cookies)
     mine_teachs = Teach.query.filter_by(instructor_id=current_user.id).all()
     if len(mine_teachs) == 0:
         return jsonify({'message':'no class'}), 404
     result = []
     for mine_cla in mine_teachs:
-        class_id = mine_class.class_id
+        class_id = mine_cla.class_id
         re = {'class_id':class_id,
               'course_id':mine_cla.classes.course.id,
               'course_name':mine_cla.classes.course.name}
