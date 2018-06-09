d74e4d94d8004c74775b5404cbf0a22f45909a70
Keyboard L <keyboard-w@outlook.com>
2018-06-09 17:41:14 +0800

    hhh

--- server/app/admin/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-admin = Blueprint('admin', __name__)
-
-from . import views
-
--- server/app/teacher/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-teacher = Blueprint('teacher', __name__)
-
-from . import views
